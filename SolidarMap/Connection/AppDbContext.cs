d391da7c30b77639a2effc895dd81849f836a287
P3terHenry <pedro.antonieti@outlook.com>
2025-06-08 21:46:09 -0300

    Adicionado AppDbContext de Entity Framework Core.

---
+++ SolidarMap/Connection/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolidarMap.Models;
+
+namespace SolidarMap.Connection
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Ajuda> Ajudas { get; set; }
+        public DbSet<TipoUsuario> TiposUsuarios { get; set; }
+        public DbSet<TipoRecurso> TiposRecursos { get; set; }
+        public DbSet<TipoZona> tipoZonas { get; set; }
+        public DbSet<Localizacao> Localizacoes { get; set; }
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+        public DbSet<Mensagem> Mensagens { get; set; }
+
+
+    }
+}
